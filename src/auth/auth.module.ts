4e12854b8c10d5d2d93320522aa9e76ac599a194
Diwakar Jha <diwakarjha554@gmail.com>
2024-10-07 01:57:55 +0530

    user created successfully

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -4,10 +4,12 @@ import { AuthController } from './auth.controller';
 import { ConfigModule } from '@nestjs/config';
 import googleOauthConfig from './config/google-oauth.config';
 import { GoogleStrategy } from './strategies/google.strategy';
+import { UserModule } from 'src/user/user.module';
 
 @Module({
   imports: [
     ConfigModule.forFeature(googleOauthConfig),
+    UserModule
   ],
   controllers: [AuthController],
   providers: [AuthService, GoogleStrategy],
