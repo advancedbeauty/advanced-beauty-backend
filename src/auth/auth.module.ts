8c1c84cb9601dfd88eb60a071da8fdd069ca4973
Diwakar Jha <diwakarjha554@gmail.com>
2024-10-09 10:06:14 +0530

    base setup

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -3,9 +3,11 @@ import { AuthService } from './auth.service';
 import { AuthController } from './auth.controller';
 import { UsersService } from '../users/users.service';
 import { PrismaService } from '../prisma/prisma.service';
+import { LocalStrategy } from './strategies/local.strategy';
 
 @Module({
+  imports: [],
   controllers: [AuthController],
-  providers: [AuthService, UsersService, PrismaService],
+  providers: [AuthService, UsersService, PrismaService, LocalStrategy],
 })
 export class AuthModule {}
--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,5 +1,6 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, UnauthorizedException } from '@nestjs/common';
 import { UsersService } from '../users/users.service';
+import { compare } from 'bcryptjs';
 
 @Injectable()
 export class AuthService {
@@ -8,6 +9,14 @@ export class AuthService {
     ) {}
 
     async validateUser(email: string, password: string) {
-
+        const user = await this.userService.findByEmail(email);
+        if(!user) {
+            throw new UnauthorizedException("User not found!");
+        }
+        const isPasswordMatch = await compare(password, user.password);
+        if(!isPasswordMatch) {
+            throw new UnauthorizedException("Invalid credentials");
+        }
+        return { id: user.id };
     }
 }
