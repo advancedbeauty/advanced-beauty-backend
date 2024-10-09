630504c3e422cb96eeb3365cdad616c8aeeee236
Diwakar Jha <diwakarjha554@gmail.com>
2024-10-09 07:40:40 +0530

    base setup

--- src/users/users.controller.ts
+++ src/users/users.controller.ts
@@ -1,15 +1,16 @@
-import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus } from '@nestjs/common';
+import { Controller, Get, Post, Body, Patch, Param, Delete, HttpException, HttpStatus, UsePipes } from '@nestjs/common';
 import { UsersService } from './users.service';
 import { User } from '@prisma/client';
-import { ZodValidationPipe } from 'src/pipes/zodValidationPipe';
-import { userSchema, userUpdateSchema, UserZodDto, UserUpdateDto } from '../zod/user.zod';
+import { ZodValidationPipe } from '../pipes/zodValidationPipe';
+import { userSchema, userUpdateSchema, UserZodDto, UserUpdateDto } from 'src/zod/user.zod';
 
 @Controller('users')
 export class UsersController {
   constructor(private readonly usersService: UsersService) {}
 
   @Post()
-  async create(@Body(new ZodValidationPipe(userSchema)) createUserDto: UserZodDto): Promise<User> {
+  @UsePipes(new ZodValidationPipe(userSchema))
+  async create(@Body() createUserDto: UserZodDto): Promise<User> {
     try {
       return await this.usersService.create(createUserDto);
     } catch (error) {
@@ -32,10 +33,8 @@ export class UsersController {
   }
 
   @Patch(':id')
-  async update(
-    @Param('id') id: string,
-    @Body(new ZodValidationPipe(userUpdateSchema)) updateUserDto: UserUpdateDto
-  ): Promise<User> {
+  @UsePipes(new ZodValidationPipe(userUpdateSchema))
+  async update(@Param('id') id: string, @Body() updateUserDto: UserUpdateDto): Promise<User> {
     try {
       return await this.usersService.update(id, updateUserDto);
     } catch (error) {
